29388233f8697dde6ddfd09cd3a1928d415623d7
devin-exe <devinkelley4@icloud.com>
2025-10-05 15:19:56 -0400

    Update script.bat
    change directory to current folder

--- script.bat
+++ script.bat
@@ -1,6 +1,6 @@
 @echo off
 setlocal enabledelayedexpansion
-
+cd /d "%~dp0"
 :: #############################################################################
 :: #                                                                           #
 :: #                  CyberPatriot Security Script                             #
